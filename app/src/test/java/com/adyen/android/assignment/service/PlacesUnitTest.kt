6fa9b390d91c80734b14030c1a196a9b7caf0afb
isoguzay <isoguzay@gmail.com>
2022-04-21 01:23:23 +0300

    Refactor api service unit test cases

--- app/src/test/java/com/adyen/android/assignment/service/PlacesUnitTest.kt
+++ app/src/test/java/com/adyen/android/assignment/service/PlacesUnitTest.kt
@@ -6,8 +6,9 @@ import org.junit.Assert.*
 import org.junit.Test
 
 class PlacesUnitTest {
+
     @Test
-    fun testResponseCode() {
+    fun verify_api_service_return_error_body_is_null() {
         val query = VenueRecommendationsQueryBuilder()
             .setLatitudeLongitude(52.376510, 4.905890)
             .build()
@@ -17,9 +18,31 @@ class PlacesUnitTest {
 
         val errorBody = response.errorBody()
         assertNull("Received an error: ${errorBody?.string()}", errorBody)
+    }
+
+    @Test
+    fun verify_api_service_return_response_is_null() {
+        val query = VenueRecommendationsQueryBuilder()
+            .setLatitudeLongitude(52.376510, 4.905890)
+            .build()
+        val response = PlacesService.instance
+            .getVenueRecommendations(query)
+            .execute()
 
         val responseWrapper = response.body()
         assertNotNull("Response is null.", responseWrapper)
+    }
+
+    @Test
+    fun verify_api_service_return_response_code_success() {
+        val query = VenueRecommendationsQueryBuilder()
+            .setLatitudeLongitude(52.376510, 4.905890)
+            .build()
+        val response = PlacesService.instance
+            .getVenueRecommendations(query)
+            .execute()
+
         assertEquals("Response code", 200, response.code())
     }
+
 }
