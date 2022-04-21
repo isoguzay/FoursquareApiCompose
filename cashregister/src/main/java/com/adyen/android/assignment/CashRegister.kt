43651a7bfa655607d4ec3677f15682076195abee
isoguzay <isoguzay@gmail.com>
2022-04-21 13:26:50 +0300

    Add amountPaid to cash register init value

--- cashregister/src/main/java/com/adyen/android/assignment/CashRegister.kt
+++ cashregister/src/main/java/com/adyen/android/assignment/CashRegister.kt
@@ -20,11 +20,11 @@ class CashRegister(private val change: Change) {
      * @throws TransactionException If the transaction cannot be performed.
      */
     fun performTransaction(price: Long, amountPaid: Change): Change {
-
         return if (amountPaid.total >= price) {
             var moneyRefund = amountPaid.total - price
             if (moneyRefund == 0L) return Change.none()
             else {
+                addAmountPaidToCashRegister(amountPaid)
                 val changeResult = Change()
                 val availableMonetaryElements = change.getElements()
                 var index = change.getElements().size - 1
@@ -37,7 +37,7 @@ class CashRegister(private val change: Change) {
                         val moneyCount = moneyRefund.toDouble() / currentMoney.minorValue.toDouble()
 
                         if (moneyCount >= 1) {
-                            val withdrawMoneyCount =min(moneyCount.toInt(), change.getCount(currentMoney))
+                            val withdrawMoneyCount = min(moneyCount.toInt(), change.getCount(currentMoney))
                             changeResult.add(currentMoney, withdrawMoneyCount)
                             change.remove(currentMoney, withdrawMoneyCount)
                             moneyRefund -= withdrawMoneyCount * currentMoney.minorValue
@@ -53,7 +53,24 @@ class CashRegister(private val change: Change) {
         } else {
             throw TransactionException("Shopper should pay more than product price!")
         }
+    }
 
+    /**
+     * Add amountPaid to Cash Register before change
+     */
+    private fun addAmountPaidToCashRegister(amountPaid: Change) {
+        val amountPaidElements = amountPaid.getElements()
+        var index = amountPaid.getElements().size - 1
+
+        while (0 <= index) {
+            val currentMoney = amountPaidElements.elementAt(index)
+            var currentMoneyIndex = amountPaid.getCount(currentMoney)
+            while (0 < currentMoneyIndex) {
+                change.add(currentMoney, currentMoneyIndex)
+                currentMoneyIndex--
+            }
+            index--
+        }
     }
 
     class TransactionException(message: String, cause: Throwable? = null) :
